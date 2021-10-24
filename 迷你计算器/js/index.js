7d0664146b1eb45ed841ff466ad8a53f3e02fea1
侯志强 <1455155765@qq.com>
2021-10-24 21:20:55 +0800

    Add files via upload
    添加学习Vue时编写的史今查询、迷你计算器、手机号查询、习惯打卡的代码

---
+++ 迷你计算器/js/index.js
@@ -0,0 +1,38 @@
+
+
+//计算器结果框组件
+const resultBox = {
+    template : `
+    
+    `
+}
+
+
+//vue实例
+const app = new Vue({
+    el : '#app',
+    data () {
+        resultValue : 0
+    },
+
+    //组件
+    components : {
+        //计算器结果框组件
+        'result-box' :
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
